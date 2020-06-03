a68f3bdfda335def3fdb6bd29d363258eaba3375
MattDavis <matthew.davis010@gmail.com>
2020-06-02 20:40:08 -0400

    finally fixed build system

--- src/Lints.jl
+++ src/Lints.jl
@@ -1,11 +1,8 @@
 module Lints
 using CxxWrap
-#using Libdl
 using libint_jll
 @wrapmodule(joinpath(dirname(pathof(Lints)),"../deps/lib/libLints.so"),:Libint2)
 function __init__()
-    #libint =  dlopen(libint_jll.Libint2_path)
     @initcxx
-#    println(libint_jll.Libint2_path)
 end
 end # module
