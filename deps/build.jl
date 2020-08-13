b5bcb4a8648fce2061924602028d899890ad2e3e
Matt Davis <matthew.davis010@gmail.com>
2020-08-13 19:01:07 -0400

    cleanup

--- deps/build.jl
+++ deps/build.jl
@@ -9,11 +9,7 @@ boost_inc = joinpath(boost_jll.artifact_dir,"include/")
 
 run(`rm -rf CMakeCache.txt CMakeFiles lib/libLints."*"`)
 run(pipeline(`sed "s|JLLPATH|$path|g" _CMakeLists.txt`,`tee CMakeLists.txt`))
-#if Sys.isapple()
-run(`sed -i\"\" "s|EIGENPATH|$eig_inc|g" CMakeLists.txt`)
-run(`sed -i\"\" "s|BOOSTPATH|$boost_inc|g" CMakeLists.txt`)
-#else
-#    run(`sed -i "s|EIGENPATH|$eig_inc|g" CMakeLists.txt`)
-#    run(`sed -i "s|BOOSTPATH|$boost_inc|g" CMakeLists.txt`)
+run(`sed -i "s|EIGENPATH|$eig_inc|g" CMakeLists.txt`)
+run(`sed -i "s|BOOSTPATH|$boost_inc|g" CMakeLists.txt`)
 run(`cmake -DCMAKE_PREFIX_PATH=$pref .`)
 run(`cmake --build .`)
